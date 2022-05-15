789d5e54a690e49e55640b90e653bb0bb128323f
赵杰裔 <zhaojieyi@bytedance.com>
2022-05-15 17:24:14 +0800

    test & bench ready

---
+++ bench/falgo_bench.cpp
@@ -0,0 +1,26 @@
+//
+// Created by zhaojieyi on 2022/5/15.
+//
+#include "benchmark/benchmark.h"
+#include "falgo.h"
+
+static void BM_SimpleRecursion(benchmark::State& state) {
+    for (auto _ : state) {
+        for (ll i = 0; i < 21; i++) {
+            falgo::simple_recursion(i);
+        }
+    }
+}
+
+static void BM_FactNaive(benchmark::State& state) {
+    for (auto _ : state) {
+        for (ll i = 0; i < 21; i++) {
+            falgo::fact_naive(i);
+        }
+    }
+}
+
+BENCHMARK(BM_SimpleRecursion);
+BENCHMARK(BM_FactNaive);
+
+BENCHMARK_MAIN();
---
+++ container/falgo.h
@@ -0,0 +1,11 @@
+#ifndef CONTAINER_FALGO_H
+#define CONTAINER_FALGO_H
+
+#define ll long long
+
+namespace falgo {
+    ll simple_recursion(ll n);
+    ll fact_naive(ll n);
+}
+
+#endif // CONTAINER_FALGO_H
