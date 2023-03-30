839b822c046e92df3e6880d9981fb9854fd310a2
赵杰裔 <zhaojieyi@bytedance.com>
2023-03-30 11:04:14 +0800

    add proc ops util func

---
+++ proc_ops/proc_util.h
@@ -0,0 +1,12 @@
+//
+// Created by zhaojieyi on 2023/3/30.
+//
+
+#ifndef CODESNIPPET_PROC_UTIL_H
+#define CODESNIPPET_PROC_UTIL_H
+
+#include <unistd.h>
+
+size_t physical_memory_used_by_process();
+
+#endif // CODESNIPPET_PROC_UTIL_H
