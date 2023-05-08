9316f671b09d309b0ea06a74967ed46ec0bdcf7b
赵杰裔 <zhaojieyi@bytedance.com>
2023-05-08 10:28:55 +0800

    start timer

---
+++ timer/TimerBase.h
@@ -0,0 +1,13 @@
+//
+// Created by zhaojieyi on 2023/5/8.
+//
+
+#ifndef CODESNIPPET_TIMERBASE_H
+#define CODESNIPPET_TIMERBASE_H
+
+class TimerBase {
+public:
+  virtual void Timeout() = 0;
+};
+
+#endif // CODESNIPPET_TIMERBASE_H
